90fdc5142ad403f282fe834ba98e35ca0b373b93
Jennifer Okeke <aestheticjenny04@gmail.com>
2024-10-02 18:17:49 +0100

    set up the environment

---
+++ src/main/java/jennifer/SelfSaga/SelfSagaApplication.java
@@ -0,0 +1,13 @@
+package jennifer.SelfSaga;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SelfSagaApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SelfSagaApplication.class, args);
+	}
+
+}
---
+++ src/test/java/jennifer/SelfSaga/SelfSagaApplicationTests.java
@@ -0,0 +1,13 @@
+package jennifer.SelfSaga;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SelfSagaApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
