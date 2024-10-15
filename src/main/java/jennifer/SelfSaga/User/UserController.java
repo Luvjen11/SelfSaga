654951088c6cae6b382c89e912bcea5e6ca45ec1
Jennifer Okeke <aestheticjenny04@gmail.com>
2024-10-15 21:22:45 +0100

    create User controller class and implement POST registerUser with exception for duplicates

---
+++ src/main/java/jennifer/SelfSaga/User/UserController.java
@@ -0,0 +1,30 @@
+package jennifer.SelfSaga.User;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+
+
+@RestController
+@RequestMapping("/selfsaga/users")
+public class UserController {
+
+    @Autowired
+    private UserService userService;
+
+    @PostMapping("/register")
+    public ResponseEntity<User> registerUser(@RequestBody User user) {
+        try {
+            User registeredUser = userService.registerUser(user);
+            return new ResponseEntity<User>(registeredUser, HttpStatus.CREATED);
+        } catch (RuntimeException e) {
+            return new ResponseEntity<>(null, HttpStatus.BAD_REQUEST);
+        }
+    }
+
+
+}
