54f398dc4f4edf40fb79257c24a677247e37c6ca
Jennifer Okeke <aestheticjenny04@gmail.com>
2024-10-22 13:11:45 +0100

    create repository interface for goals

---
+++ src/main/java/jennifer/SelfSaga/Goal/GoalRepository.java
@@ -0,0 +1,11 @@
+package jennifer.SelfSaga.Goal;
+
+import java.util.List;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface GoalRepository extends JpaRepository<Goal, Long> {
+
+    List<Goal> findByUserId(Long userId);  // Get goals for a specific user by id
+    List<Goal> findByUsername(String username);  // Get goals for a specific user by username
+
+}
