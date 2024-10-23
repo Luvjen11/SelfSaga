1d379b552d5ec22bf4b1556d78c14c0816d7e570
Jennifer Okeke <aestheticjenny04@gmail.com>
2024-10-23 16:05:35 +0100

    create controllor method for creating tasks

--- src/main/java/jennifer/SelfSaga/Task/TaskController.java
+++ src/main/java/jennifer/SelfSaga/Task/TaskController.java
@@ -5,6 +5,8 @@ import java.util.List;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -25,6 +27,18 @@ public class TaskController {
     @GetMapping("/tasks")
     public List<Task> getTasksByUsername(@PathVariable String username) {
         return taskService.getAllTasksByUsername(username);
+    }
+
+    //create task for specific goal
+    @PostMapping("/goals/{goalId}/tasks") 
+    public Task createTask(@PathVariable Long goalId, @RequestBody Task task) {
+        return taskService.createTask(task, goalId);
+    }
+
+    //create a single task 
+    @PostMapping("/tasks")
+    public Task singleTask(@RequestBody Task task) {
+        return taskService.singleTask(task);
     }
 
 
