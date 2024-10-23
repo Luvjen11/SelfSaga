ab760ad2ebf543e14fd538ccd84bdd751fbb2cb0
Jennifer Okeke <aestheticjenny04@gmail.com>
2024-10-23 12:58:07 +0100

    add findTaskByGoal method in service class

--- src/main/java/jennifer/SelfSaga/Task/TaskRepository.java
+++ src/main/java/jennifer/SelfSaga/Task/TaskRepository.java
@@ -1,8 +1,11 @@
 package jennifer.SelfSaga.Task;
 
+import java.util.List;
+
 import org.springframework.data.jpa.repository.JpaRepository;
 
+
 public interface TaskRepository extends JpaRepository<Task, Long> {
-
-
+     
+    List<Task> findByGoalId(Long goalId);
 } 
--- src/main/java/jennifer/SelfSaga/Task/TaskService.java
+++ src/main/java/jennifer/SelfSaga/Task/TaskService.java
@@ -1,5 +1,7 @@
 package jennifer.SelfSaga.Task;
 
+import java.util.List;
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
@@ -15,10 +17,17 @@ public class TaskService {
     @Autowired
     private GoalRepository goalRepository;
 
+    //get task for specifi goal
+    public List<Task> getTasksByGoal(Long goalId) {
+        return taskRepository.findByGoalId(goalId);
+    }
+
     //create task for specific goal
     public Task createTask(Task task, Long goalId) {
         Goal goal = goalRepository.findById(goalId).orElseThrow(() -> new IllegalArgumentException("Goal not found with id: " + goalId));
         task.setGoal(goal);
         return taskRepository.save(task);
     }
+
+    
 }
