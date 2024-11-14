91c2b47caef688d8abfe006313f12a4a4a3fac8d
Jennifer Okeke <aestheticjenny04@gmail.com>
2024-11-14 19:49:40 +0000

    create visionBoard entity class

---
+++ src/main/java/jennifer/SelfSaga/VisionBoard/visionBoard.java
@@ -0,0 +1,50 @@
+package jennifer.SelfSaga.VisionBoard;
+
+
+
+import java.sql.Blob;
+import java.time.LocalDate;
+
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.JoinColumn;
+import jakarta.persistence.Table;
+import jennifer.SelfSaga.User.User;
+
+@Entity
+@Table(name="visionboard")
+public class visionBoard {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+
+    private Long id;
+    private Blob image;
+    private LocalDate createdAt;
+       
+    @JoinColumn(name = "user_id",nullable = false)
+    private User user;
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public Blob getImage() {
+        return image;
+    }
+
+    public void setImage(Blob image) {
+        this.image = image;
+    }
+
+    public LocalDate getCreatedAt() {
+        return createdAt;
+    }
+
+}
